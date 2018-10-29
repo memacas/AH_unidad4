b5c4ffe83a8642463f170a6c6e12ae96890e5b76
memacas <mejoresmarcas.com>
2018-10-29 14:24:31 -0500

    Recepcion de numeros en el display

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var calculadora = function(){
   var teclas = document.querySelectorAll(".tecla");
 	function init(){
     for(var x=0; x<teclas.length; x++){
+      //Agregamos los listener a las teclas de clase "tecla"
         teclas[x].addEventListener("click", obtener_tecla);
         teclas[x].addEventListener("mousedown", function(){this.style = "transform:scale(0.5,0.5)";});
         teclas[x].addEventListener("mouseup", function(){this.style = "transform:scale(1,1)";});
@@ -9,14 +10,26 @@ var calculadora = function(){
 	}
 
   function obtener_tecla(){
+    var resultado = document.querySelector("#display").textContent;
     var tecla_presionada = this.id;
-    switch (tecla_presionada) {
-      case "on":
-        document.querySelector("#display").innerHTML  = "";
-        break;
-      default:
+
+    if (!isNaN(tecla_presionada)){
+        if (resultado == 0 && tecla_presionada != 0){
+          //alert(tecla_presionada);
+          document.querySelector("#display").innerHTML  = tecla_presionada;
+        }else{
+          document.querySelector("#display").innerHTML  = parseFloat(tecla_presionada) + resultado;
+        }
+    }else{
+      switch (tecla_presionada) {
+        case "on":
+          document.querySelector("#display").innerHTML  = "0";
+          break;
+        default:
+      }
     }
   }
+  //Inicializamos por default la function init()
   init();
 }
 
