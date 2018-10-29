2312e46c26dc783172f0df2bc9a3cb48cbf36d7b
memacas <mejoresmarcas.com>
2018-10-29 13:21:34 -0500

    Creacion del modulo Calculadora

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
-var calculadora
+var calculadora = function(){
+  var teclas = document.querySelectorAll(".tecla");
+	function init(){
+    for(var x=0; x<teclas.length; x++){
+        teclas[x].addEventListener("click", obtener_tecla);
+        teclas[x].addEventListener("mousedown", function(){this.style = "transform:scale(0.5,0.5)";});
+        teclas[x].addEventListener("mouseup", function(){this.style = "transform:scale(1,1)";});
+    }
+	}
 
-function obtener_tecla(){
-  var tecla_presionada = this.id;
-  switch (tecla_presionada) {
-    case "on":
-      document.querySelector("#display").innerHTML  = "0";
-      break;
-    default:
-
+  function obtener_tecla(){
+    var tecla_presionada = this.id;
+    switch (tecla_presionada) {
+      case "on":
+        document.querySelector("#display").innerHTML  = "";
+        break;
+      default:
+    }
   }
+  init();
 }
 
-var teclas = document.querySelectorAll(".tecla");
-for(var x=0; x<teclas.length; x++){
-    teclas[x].addEventListener("click", obtener_tecla);
-    teclas[x].addEventListener("mousedown", function(){this.style = "transform:scale(0.5,0.5)";});
-    teclas[x].addEventListener("mouseup", function(){this.style = "transform:scale(1,1)";});
-}
+var MiCalculadora = new calculadora();
